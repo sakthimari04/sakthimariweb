db31007c9ba281e0f81e24f65e543b6c4751f36f
sakthimari04 <sakthimari04@outlook.com>
2024-06-20 07:18:34 +0530

    Setup for GitHub Pages deployment

---
+++ precache-manifest.1b571f760c65caa9ab05a8080663246b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a6f1e3f29528ffd67986072950a2246",
+    "url": "/sakthimariweb/index.html"
+  },
+  {
+    "revision": "1779032d1d9fde15801d",
+    "url": "/sakthimariweb/static/css/2.98036eff.chunk.css"
+  },
+  {
+    "revision": "5ad91bed086c483c31ce",
+    "url": "/sakthimariweb/static/css/main.b4f05c9b.chunk.css"
+  },
+  {
+    "revision": "1779032d1d9fde15801d",
+    "url": "/sakthimariweb/static/js/2.a5213630.chunk.js"
+  },
+  {
+    "revision": "c22e7b4cb20d4556252c2aa78698d764",
+    "url": "/sakthimariweb/static/js/2.a5213630.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ad91bed086c483c31ce",
+    "url": "/sakthimariweb/static/js/main.bc577fe6.chunk.js"
+  },
+  {
+    "revision": "42356516a5b2f2c5fd08",
+    "url": "/sakthimariweb/static/js/runtime-main.609d3b58.js"
+  },
+  {
+    "revision": "a140506ae98711a551e477b8cf193184",
+    "url": "/sakthimariweb/static/media/background.a140506a.jpg"
+  },
+  {
+    "revision": "2e155eb8fed0df5c2f86919125c21e39",
+    "url": "/sakthimariweb/static/media/welcome-background.2e155eb8.jpg"
+  }
+]);
